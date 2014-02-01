061259648c23a5a29ac97733a0b1ad934cbc0cf5
Mihail Velikov <mihailvelik@gmail.com>
2014-02-01 22:32:09 +0200

    create app Model classes

---
+++ Flat Tracks/Route.m
@@ -0,0 +1,20 @@
+//
+//  Route.m
+//  Flat Tracks
+//
+//  Created by mihata on 2/1/14.
+//  Copyright (c) 2014 mihata. All rights reserved.
+//
+
+#import "Route.h"
+#import "Point.h"
+
+
+@implementation Route
+
+@dynamic start;
+@dynamic title;
+@dynamic end;
+@dynamic points;
+
+@end
