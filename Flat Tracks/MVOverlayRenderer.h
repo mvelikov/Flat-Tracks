30ddb7ada0b556703586f1e1e0f47daeda7af1fd
Mihail Velikov <mihailvelik@gmail.com>
2014-02-06 23:53:53 +0200

    remove some files

--- Flat Tracks/MVOverlayRenderer.h
+++
@@ -1,7 +0,0 @@
-
-#import <Foundation/Foundation.h>
-@import MapKit;
-
-@interface MVOverlayRenderer : MKOverlayRenderer;
-- (id) initWithOverlay:(id <MKOverlay>)overlay angle: (CGFloat) angle;
-@end
